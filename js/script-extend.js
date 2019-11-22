305b9480f80532f38b57a8fc67b524a1e4ec159d
eridhobffry <eridhobffry@github.com>
2019-11-22 11:15:56 +0100

    Make logo brand js

---
+++ js/script-extend.js
@@ -0,0 +1,41 @@
+/**
+ * Global variables
+ */
+"use strict";
+
+var userAgent = navigator.userAgent.toLowerCase(),
+	initialDate = new Date(),
+
+	$document = $(document),
+	$window = $(window),
+	$html = $("html"),
+
+	isDesktop = $html.hasClass("desktop"),
+	isIE = userAgent.indexOf("msie") != -1 ? parseInt(userAgent.split("msie")[1]) : userAgent.indexOf("trident") != -1 ? 11 : userAgent.indexOf("edge") != -1 ? 12 : false,
+	isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+	isTouch = "ontouchstart" in window,
+	c3ChartsArray = [],
+	onloadCaptchaCallback,
+
+	plugins = {
+		rdNavbar: $(".rd-navbar"),
+    };
+    
+$document.ready(function () { 
+
+    /**
+	 * RD Navbar
+	 * @description Enables RD Navbar plugin
+	 */
+	if (plugins.rdNavbar.length) {
+		plugins.rdNavbar.RDNavbar({
+			stickUpClone: (plugins.rdNavbar.attr("data-stick-up-clone")) ? plugins.rdNavbar.attr("data-stick-up-clone") === 'true' : false,
+			stickUpOffset: (plugins.rdNavbar.attr("data-stick-up-offset")) ? plugins.rdNavbar.attr("data-stick-up-offset") : 1,
+			anchorNavOffset: -120
+		});
+		if (plugins.rdNavbar.attr("data-body-class")) {
+			document.body.className += ' ' + plugins.rdNavbar.attr("data-body-class");
+		}
+	}
+})
+    
